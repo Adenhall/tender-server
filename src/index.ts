864232be9de72f86c444c0e94533874275ebea6f
Quan Nguyen <nguyen.mq@allerin.nl>
2021-06-01 19:26:42 +0700

    first routing

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
-const express = require("express");
+import express from "express";
+const cors = require("cors");
+import authRouter from "./routes/auth";
 
 const main = () => {
   const app = express();
+  app.use(cors());
+
+  app.use("/auth", authRouter);
 
   const PORT = process.env.PORT || 3000;
-
   app.listen(PORT, () => {
     if (process.env.NODE_ENV !== "production") {
       console.log(`server is listening to localhost:${PORT}`);
